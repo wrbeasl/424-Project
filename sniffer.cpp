b9c59e248732fe6fea79e4da9539a672f999e41a
wrbeasl <wrbeasl@g.clemson.edu>
2015-04-01 11:33:50 -0400

    Updated names and removed unneeded files

--- sniffer.cpp
+++ sniffer.cpp
@@ -1,34 +1,70 @@
+#include <vector>
+#include <string>
 #include <tins/tins.h>
+#include <iostream>
 
 using namespace Tins;
 
-bool doo(PDU&) {
-    return false;
+// This is a handler used in Sniffer::sniff_loop
+bool handler(const PDU& pkt) {
+
+    // Lookup the UDP PDU
+    const UDP &udp = pkt.rfind_pdu<UDP>();
+
+    if(udp.sport() == 53 || udp.dport() == 53) {
+
+        DNS dns = pkt.rfind_pdu<RawPDU>().to<DNS>();
+        IP ip = pkt.rfind_pdu<RawPDU>().to<IP>();
+
+
+
+
+        for(const auto &query : dns.queries()){
+            std::cout << "New Connection found. \n "
+                      << "--------------------- \n ";
+
+            std::cout << "Source IP: " << ip.src_addr();
+            std::cout << " Destination IP: " << ip.dst_addr();
+            std::cout << " DNS Name: ";
+            std::cout << query.dname() << std::endl;
+        }
+
+        for(const auto &rsc : dns.answers()){
+            std::cout << "Packet data: " << rsc.data() << std::endl;
+            std::cout << "TTL Field data: " << rsc.ttl() << std::endl;
+        }
+    }
+    return true;
 }
 
-struct foo {
-    void bar() {
-        SnifferConfiguration config;
-        config.set_promisc_mode(true);
-        config.set_filter("ip src 192.168.0.100");
-        Sniffer sniffer("wlan0", config);
-        /* Uses the helper function to create a proxy object that
-         * will call this->handle. If you're using boost or C++11,
-         * you could use boost::bind or std::bind, that will also
-         * work.
-         */
-        sniffer.sniff_loop(make_sniffer_handler(this, &foo::handle));
-        // Also valid
-        sniffer.sniff_loop(doo);
-    }
-    
-    bool handle(PDU&) {
-        // Don't process anything
-        return false;
-    }
-};
-
 int main() {
-    foo f;
-    f.bar();
+    std::string Wire;
+    std::string Port;
+
+    std::cout << "Hello and welcome to the Packet Sniffer\n";
+    std::cout << "Please give which wire to sniff on: \n";
+    std::cin >> Wire;
+    std::vector<Packet> vt;
+
+    SnifferConfiguration config;
+    config.set_promisc_mode(true);
+    
+    Sniffer sniffer(Wire, config);
+    sniffer.sniff_loop(handler);
+    /*
+    while(vt.size() != 10)
+        // next_packet returns a PtrPacket, which can be implicitly converted to Packet.
+        vt.push_back(sniffer.next_packet());
+    // Done, now let's check the packets
+    for(const auto& packet : vt) {
+        // Is there an IP PDU somewhere?
+        if(packet.pdu()->find_pdu<IP>()) {
+            // Just print timestamp's seconds and IP source address
+            std::cout << "At: " << packet.timestamp().seconds()
+                    << " - " << packet.pdu()->rfind_pdu<IP>().src_addr() 
+                    << std::endl;
+
+            //handler(packet.pdu()->find_pdu<IP>());
+        }
+    } */
 }
